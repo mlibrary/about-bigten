8761f63808373834fd181ead7274b4282f57439b
Seth A. Johnson <sethajoh@umich.edu>
2025-12-12 12:13:45 -0500

    HELIO-4990 adjust layout of cards to allow more words in buttons

--- src/components/cards/card.js
+++ src/components/cards/card.js
@@ -14,11 +14,13 @@ const Card = ({cardImage, card}) => {
     <div className="card col-md-5 mb-3">      
       <img src={cardImage} alt={`${title}`} className="card-img" />
       <div className="card-body row">
-        <div className="col-sm-8">
+        <div className="col-sm-12">
           <h3 className="card-title">{title}</h3>
           <p className="card-text">{description}</p>
         </div>
-        <div className="col-xs-4">
+      </div>
+      <div className="card-body row">
+        <div className="col-xs-12">
           {
             buttonUrl.startsWith("/")
             ? <Link className="card-link btn btn-secondary" role="button" to={buttonUrl}>{buttonLabel}</Link>
